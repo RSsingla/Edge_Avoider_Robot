ab826539e2b7d5b92063709e68c6cf38946fd38a
RSsingla <raghavsingla052@gmail.com>
2018-08-31 19:02:34 +0530

    hi

---
+++ Edge_Avoider_Robot.c
@@ -0,0 +1,112 @@
+// Project name 	: EDGE_AVOIDER
+// Designed By 		: Entrench Electronics
+// Designed for		: Entrench Electronics
+//                	  http://entrench.in
+
+/* Program for "EDGE AVOIDER/Fabricated for avoiding edges of a table" 
+______________________________________________________________________
+
+Connection Settings of the Kit
+
+LEDs---@------------->PB1
+LEDs---@------------->PB2
+LEDs---@------------->PB3
+LEDs---@------------->PB4
+left sensor---------->PC3
+right sensor--------->PC0
+SOUND SENSOR--------->PB0
+RIGHT MOTOR(+)------->PB1
+RIGHT MOTOR(-)------->PB2
+LEFT MOTOR(-)-------->PB3
+LEFT MOTOR(+)-------->PB4
+BOOTLoader Condition Check----->PC5(If 0 bootloader section else program execution
+							    section of Flash memory)
+RESET-------------------------->PC6
+Crystal Oscillator------------->PB6 and PB7
+VB=Battery Supply
+VCC=regulated 5V+
+Gnd=Ground(0V)
+*/
+
+#include<avr/io.h>
+#include<util/delay.h>
+#include "Entrench.c"
+int main(void)
+{
+	  	
+	DDRC=0b0000000;   			//set PORTC as input port
+	DDRB=0b00011110;  			//PB1, PB2, PB3, PB4 as output port
+    int ls=0, rs=0;   			// define & initialize ls, rs integer as 0 to 
+					  			// acquire the left sensor status in ls and right sensor
+					  			// status in rs 
+	while(1)          			// create infinite loop
+	{
+	
+	rs=(PINC&0b0000001);   		//acquire only left sensor status connected at PC0
+	ls=(PINC&0b0001000);   		// acquire only right sensor status connected at PC3
+
+		
+		if((rs==0b0000000)||(ls==0b0000000)) 
+		{
+	
+		}		
+
+		if((rs==0b0000000)&&(ls==0b0000000)) //check sensor status for both sensor OFF 
+		{
+
+			PORTB=0b00000000;  	//stop
+		
+			PORTB=0b00001100;  	//backward
+			wait(.8);
+     		PORTB=0b00000010;  	//turn right or user can define their own turn
+			wait(.8);
+			ls=0;              	//set sensor status off
+			rs=0;              	//set sensor status off
+		
+		}
+
+		if((rs==0b0000001)&&(ls==0b0000000))  //check sensor status for left sensor=ON and
+		                                     // right sensor=OFF
+		{
+			
+		
+			PORTB=0b00001100;    //backward
+			wait(.8);
+			PORTB=0b00010000;    //turn right to avoid the edge
+			wait(.8);
+			ls=0;                //set sensor status off
+			rs=0; 				 //set sensor status off
+				            
+		}
+	
+	
+		if((rs==0b0000000)&&(ls==0b0001000))  //check sensor status for left sensor=OFF and
+		                                     // right sensor=ON
+		{
+			 
+		
+			PORTB=0b00001100;    //backward
+			wait(.8);
+			PORTB=0b00000010;    //turn left to avoid the edge
+			wait(.8);
+			ls=0;                //set sensor status off
+			rs=0; 				 ///set sensor status off
+			 
+		}
+	
+		
+
+        if((rs==0b0000001)&&(ls==0b0001000)) //check sensor status for both sensor ON 
+		{
+
+			PORTB=0b00010010;  //move forward
+			ls=0;              //set sensor status off
+			rs=0;              //set sensor status off
+		
+		}
+		
+		
+		
+	}
+
+}
